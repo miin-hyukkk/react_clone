25eec909f0a6ac72313db2d89031b2ace0143dd5
miin-hyukkk <ham9893@naver.com>
2022-11-27 19:41:40 +0900

    react 클론코딩 html css 구성_2

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import React, { useEffect, useState,useRef } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import axios from 'axios';
 
 
+let im = 'https://via.placeholder.com/150/92c952'
+
+axios.get('https://jsonplaceholder.typicode.com/photos/1')
+    .then((data) => {
+        console.log(data.data.thumbnailUrl)
+        const images = data.data.thumbnailUrl
+    })
 function App() {
+  // let [pImage,setImage] = useState(https://jsonplaceholder.typicode.com/photos/1);
   return (
     <div className="App">
           <Navbar className= "nav_up" bg="light" variant="light">
@@ -18,9 +27,14 @@ function App() {
                   </Nav>
           </Navbar>
       <div className="profile">
-        <div className="profileImage">프로필 </div>
-        <div className="profileInfo">정보 </div><
-        /div>
+        <div className="profileImage">
+            <img className="pImage" src='prof.jpeg'/>
+        </div>
+        <div className="profileInfo">
+            {/*<button onClick={() =>{*/}
+            {/*}}>버튼</button>*/}
+        </div>
+      </div>
           <Navbar className="nav_center" bg="light" variant="light">
                   <Nav className="me-auto">
                       <Nav.Link className="photoB"  href="#home">PHOTO</Nav.Link>
